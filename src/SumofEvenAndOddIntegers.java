e2147614a1ea7b9db7180999d94c6105122bd83b
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:51:18 -0800

    Initial commit

---
+++ src/SumofEvenAndOddIntegers.java
@@ -0,0 +1,23 @@
+public class SumofEvenAndOddIntegers {
+    public static void main(String[] args) {
+
+        //this program is to print the sum of even integers between 0 and 6
+        int a, b, c;
+        a=2;
+        b=4;
+        c=6;
+        int sum = a+b+c;
+        System.out.println(sum);
+
+
+        //this program is to print the sum of odd integers between 0 and 5
+        int d, e, f;
+        d=1;
+        e=3;
+        f=5;
+        int k = sum;
+        int sum = d+e+f;
+        System.out.println("sum of odd integers:  +");
+
+    }
+}
